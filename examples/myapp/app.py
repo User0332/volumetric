eb39183da44b27de89caee3f50e2b3219e98fb9c
Carl Furtado <carlzfurtado@gmail.com>
2026-04-12 21:07:29 -0400

    update examples

--- examples/myapp/app.py
+++ examples/myapp/app.py
@@ -2,13 +2,17 @@ from flask import request
 from volumetric import App
 import pyjsx.auto_setup
 
-app = App(__name__, secrets_path=".env")
+class PluginObjects:
+	authservice: 'AuthService'
+
+app = App[PluginObjects](__name__, secrets_path=".env")
 
 app.fs_routes.enable()
 
 class AuthService:
 	def get_user(self) -> str | None:
 		return request.args.get("user")
+
 
 app.debug = True
 
